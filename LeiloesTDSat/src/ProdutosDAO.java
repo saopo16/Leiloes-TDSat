84cf4289328d8cc03f35e4cb6fcdadc97182bdc9
Sara Portugal <saportugal2006@gmail.com>
2024-01-05 20:22:46 -0300

    botão de salvar cadastrando produto.

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -8,6 +8,8 @@
  * @author Adm
  */
 
+import java.sql.SQLException;
+import com.mysql.jdbc.Statement;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -22,13 +24,42 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public void cadastrarProduto(ProdutosDTO produto) {
+        conn = new conectaDAO().connectDB();
+
+        try {
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+            // Obtenha o ID gerado para o novo produto
+            ResultSet generatedKeys = prep.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                produto.setId(generatedKeys.getInt(1));
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+        } finally {
+            // Feche as conexões e recursos
+            try {
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                // Lidar com exceções, se necessário
+            }
+        }
     }
+
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
